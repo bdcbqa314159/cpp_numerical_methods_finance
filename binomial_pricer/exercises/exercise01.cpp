582ea1d9c2df86e2c453406d1cdac85856478b47
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2025-02-08 08:40:33 +0000

    binomial_pricer - entering data

---
+++ binomial_pricer/exercises/exercise01.cpp
@@ -0,0 +1,74 @@
+/*
+Tweak the code in Main02.cpp so
+the user can enter n and i from the keyboard.
+*/
+
+/*
+For S0=100., U = 0.9, D = 0.2, R = 0.5;
+And: n=3, i=2;
+We have:
+q=0.428571
+S(n,i )=433.2
+*/
+
+#include <iostream>
+
+int main()
+{
+    double S0, U, D, R;
+
+    std::cout << "Enter S0 ";
+    std::cin >> S0;
+    std::cout << "Enter U ";
+    std::cin >> U;
+    std::cout << "Enter D ";
+    std::cin >> D;
+    std::cout << "Enter R ";
+    std::cin >> R;
+    std::cout << "\n";
+
+    // making sure that 0<S0, -1<D<U, -1<R
+    if (S0 <= 0.0 || U <= -1.0 || D <= -1.0 || U <= D || R <= -1.0)
+    {
+        std::cout << "Illegal data ranges" << std::endl;
+        std::cout << "Terminating program" << std::endl;
+        return 1;
+    }
+
+    // checking for arbitrage
+    if (R >= U || R <= D)
+    {
+        std::cout << "Arbitrage exists" << std::endl;
+        std::cout << "Terminating program" << std::endl;
+        return 1;
+    }
+
+    std::cout << "Input data checked" << std::endl;
+    std::cout << "There is no arbitrage" << std::endl
+              << std::endl;
+    // compute risk-neutral probability
+    std::cout << " q=" << (R - D) / (U - D) << std::endl;
+    // compute stock price at node n=3,i=2
+
+    int n;
+    int i;
+
+    std::cout << "Enter n (number of steps) ";
+    std::cin >> n;
+    std::cout << "Enter i (specific node) ";
+    std::cin >> i;
+    std::cout << "\n";
+
+    if (n < i)
+    {
+        std::cout << "node i must be <= than number of steps n" << std::endl;
+        std::cout << "Terminating program" << std::endl;
+        return 1;
+    }
+
+    std::cout << " n=" << n << std::endl;
+    std::cout << " i=" << i << std::endl;
+    std::cout << "S(n,i )=" << S0 * pow(1 + U, i) * pow(1 + D, n - i) << std::endl;
+
+    return 0;
+}
