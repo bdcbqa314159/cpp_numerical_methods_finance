5ccb64e9c9370e9bfc47d23aec0a1c40362654ab
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2025-02-09 07:58:53 +0000

    binomial pricer revisited - cleanup

---
+++ binomial_pricer_revisited/our_first_class/main08.cpp
@@ -0,0 +1,32 @@
+#include "bin_model02.hpp"
+#include "options04.hpp"
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    BinModel Model;
+
+    if (Model.GetInputData() == 1)
+        return 1;
+
+    double K; // strike price
+    int N;    // steps to expiry
+
+    cout << "Enter call option data:" << endl;
+    GetInputData(N, K);
+    cout << "European call option price = "
+         << PriceByCRR(Model, N, K, CallPayoff)
+         << endl
+         << endl;
+
+    cout << "Enter put option data:" << endl;
+    GetInputData(N, K);
+    cout << "European put option price =  "
+         << PriceByCRR(Model, N, K, PutPayoff)
+         << endl
+         << endl;
+
+    return 0;
+}
