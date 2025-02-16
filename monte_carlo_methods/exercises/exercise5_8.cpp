c794abe70893f42ea8a954cd06d864b6a90b8785
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2025-02-16 09:04:54 +0000

    monte carlo methods - correction

---
+++ monte_carlo_methods/exercises/exercise5_8.cpp
@@ -0,0 +1,241 @@
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+typedef std::vector<double> SamplePath;
+const double pi = 4.0 * atan(1.0);
+
+class EurCall
+{
+public:
+    double T, K;
+    EurCall(double T_, double K_)
+    {
+        T = T_;
+        K = K_;
+    }
+    double d_plus(double S0, double sigma, double r);
+    double d_minus(double S0, double sigma, double r);
+    double PriceByBSFormula(double S0,
+                            double sigma, double r);
+    double VegaByBSFormula(double S0,
+                           double sigma, double r);
+};
+
+class BSModel
+{
+public:
+    double S0, r, sigma;
+    BSModel(double S0_, double r_, double sigma_)
+    {
+        S0 = S0_;
+        r = r_;
+        sigma = sigma_;
+        srand(time(NULL));
+    }
+    void GenerateSamplePath(double T, int m, SamplePath &S);
+};
+
+class PathDepOption
+{
+public:
+    double T, Price, PricingError;
+    int m;
+    virtual double Payoff(SamplePath &S) = 0;
+    double PriceByMC(BSModel Model, long N);
+    double PriceByVarRedMC(BSModel Model, long N, PathDepOption &CVOption);
+    virtual double PriceByBSFormula(BSModel Model) { return 0.0; }
+};
+
+class DifferenceOfOptions : public PathDepOption
+{
+public:
+    PathDepOption *Ptr1;
+    PathDepOption *Ptr2;
+    DifferenceOfOptions(double T_, int m_,
+                        PathDepOption *Ptr1_,
+                        PathDepOption *Ptr2_)
+    {
+        T = T_;
+        m = m_;
+        Ptr1 = Ptr1_;
+        Ptr2 = Ptr2_;
+    }
+    double Payoff(SamplePath &S)
+    {
+        return Ptr1->Payoff(S) - Ptr2->Payoff(S);
+    }
+};
+
+class ArthmAsianCall : public PathDepOption
+{
+public:
+    double K;
+    ArthmAsianCall(double T_, double K_, int m_)
+    {
+        T = T_;
+        K = K_;
+        m = m_;
+    }
+    double Payoff(SamplePath &S);
+};
+
+class ContGmtrAsianCall : public PathDepOption
+{
+public:
+    double K;
+    ContGmtrAsianCall(double T_, double K_, int m_)
+    {
+        T = T_;
+        K = K_;
+        m = m_;
+    }
+    double Payoff(SamplePath &S);
+    double PriceByBSFormula(BSModel Model);
+};
+
+int main()
+{
+    double S0 = 100.0, r = 0.03, sigma = 0.2;
+    BSModel Model(S0, r, sigma);
+
+    double T = 1.0 / 12.0, K = 100.0;
+
+    // For the considered integrals to be computed with sufficient
+    // accuracy, we have to choose a large m
+    int m = 300;
+
+    ArthmAsianCall Option(T, K, m);
+    ContGmtrAsianCall CVOption(T, K, m);
+
+    long N = 30000;
+    Option.PriceByVarRedMC(Model, N, CVOption);
+    cout << "Arithmetic call price = " << Option.Price << endl
+         << "Error = " << Option.PricingError << endl;
+
+    CVOption.PriceByMC(Model, N);
+    cout << "Price by direct MC = " << CVOption.Price << endl
+         << "MC Error = " << CVOption.PricingError << endl;
+
+    return 0;
+}
+
+double Gauss()
+{
+    double U1 = (rand() + 1.0) / (RAND_MAX + 1.0);
+    double U2 = (rand() + 1.0) / (RAND_MAX + 1.0);
+    return sqrt(-2.0 * log(U1)) * cos(2.0 * pi * U2);
+}
+
+void BSModel::GenerateSamplePath(double T, int m, SamplePath &S)
+{
+    double St = S0;
+    for (int k = 0; k < m; k++)
+    {
+        S[k] = St * exp((r - sigma * sigma * 0.5) * (T / m) + sigma * sqrt(T / m) * Gauss());
+        St = S[k];
+    }
+}
+
+double PathDepOption::PriceByMC(BSModel Model, long N)
+{
+    double H = 0.0, Hsq = 0.0;
+    SamplePath S(m);
+    for (long i = 0; i < N; i++)
+    {
+        Model.GenerateSamplePath(T, m, S);
+        H = (i * H + Payoff(S)) / (i + 1.0);
+        Hsq = (i * Hsq + pow(Payoff(S), 2.0)) / (i + 1.0);
+    }
+    Price = exp(-Model.r * T) * H;
+    PricingError = exp(-Model.r * T) * sqrt(Hsq - H * H) / sqrt(N - 1.0);
+    return Price;
+}
+
+double PathDepOption::PriceByVarRedMC(BSModel Model, long N, PathDepOption &CVOption)
+{
+    DifferenceOfOptions VarRedOpt(T, m, this, &CVOption);
+
+    Price = VarRedOpt.PriceByMC(Model, N) + CVOption.PriceByBSFormula(Model);
+
+    PricingError = VarRedOpt.PricingError;
+
+    return Price;
+}
+
+double ArthmAsianCall::Payoff(SamplePath &S)
+{
+    double Ave = 0.0;
+    for (int k = 0; k < m; k++)
+        Ave = (k * Ave + S[k]) / (k + 1.0);
+    if (Ave < K)
+        return 0.0;
+    return Ave - K;
+}
+
+double ContGmtrAsianCall::Payoff(SamplePath &S)
+{
+    double Ave = 0.0;
+    for (int k = 0; k < m; k++)
+    {
+        Ave = (k * Ave + log(S[k])) / (k + 1.0);
+    }
+    Ave = exp(Ave);
+    if (Ave < K)
+        return 0.0;
+    return Ave - K;
+}
+
+double ContGmtrAsianCall::PriceByBSFormula(BSModel Model)
+{
+    double c1 = Model.S0 * exp(-0.5 * (Model.r + Model.sigma * Model.sigma / 6.0) * T);
+    double c2 = Model.sigma / sqrt(3.0);
+    EurCall G(T, K);
+    Price = G.PriceByBSFormula(c1, c2, Model.r);
+    return Price;
+}
+
+double N(double x)
+{
+    double gamma = 0.2316419;
+    double a1 = 0.319381530;
+    double a2 = -0.356563782;
+    double a3 = 1.781477937;
+    double a4 = -1.821255978;
+    double a5 = 1.330274429;
+    double pi = 4.0 * atan(1.0);
+    double k = 1.0 / (1.0 + gamma * x);
+    if (x >= 0.0)
+    {
+        return 1.0 - ((((a5 * k + a4) * k + a3) * k + a2) * k + a1) * k * exp(-x * x / 2.0) / sqrt(2.0 * pi);
+    }
+    else
+        return 1.0 - N(-x);
+}
+
+double EurCall::d_plus(double S0, double sigma, double r)
+{
+    return (log(S0 / K) +
+            (r + 0.5 * pow(sigma, 2.0)) * T) /
+           (sigma * sqrt(T));
+}
+
+double EurCall::d_minus(double S0, double sigma, double r)
+{
+    return d_plus(S0, sigma, r) - sigma * sqrt(T);
+}
+
+double EurCall::PriceByBSFormula(double S0,
+                                 double sigma, double r)
+{
+    return S0 * N(d_plus(S0, sigma, r)) - K * exp(-r * T) * N(d_minus(S0, sigma, r));
+}
+
+double EurCall::VegaByBSFormula(double S0,
+                                double sigma, double r)
+{
+    double pi = 4.0 * atan(1.0);
+    return S0 * exp(-d_plus(S0, sigma, r) * d_plus(S0, sigma, r) / 2) * sqrt(T) / sqrt(2.0 * pi);
+}
